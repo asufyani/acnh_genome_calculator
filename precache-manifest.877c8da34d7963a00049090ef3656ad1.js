65f21f23820d06901a0688212dd8fadf6ebeacb7
Amir Sufyani <amir.sufyani@gmail.com>
2020-05-12 18:46:14 -0400

    Updates

---
+++ precache-manifest.877c8da34d7963a00049090ef3656ad1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ffd183c0817c3eccc0f7e1bd227b010",
+    "url": "/acnh_genome_calculator/index.html"
+  },
+  {
+    "revision": "e94faf6616c95001c159",
+    "url": "/acnh_genome_calculator/static/css/main.6ba599dc.chunk.css"
+  },
+  {
+    "revision": "3653da7c052f8a2b132f",
+    "url": "/acnh_genome_calculator/static/js/2.521700d4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/acnh_genome_calculator/static/js/2.521700d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94faf6616c95001c159",
+    "url": "/acnh_genome_calculator/static/js/main.f849dba5.chunk.js"
+  },
+  {
+    "revision": "26c3805bad76b5f1f535",
+    "url": "/acnh_genome_calculator/static/js/runtime-main.b9f1097c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/acnh_genome_calculator/precache-manifest.ec8fc9a983c950ec9026479065435a70.js"
+  "/acnh_genome_calculator/precache-manifest.877c8da34d7963a00049090ef3656ad1.js"
 );
 
 self.addEventListener('message', (event) => {
